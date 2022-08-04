042203e386f602c348a9f7c1f3ec04cb9aeb7ff2
AM111D <86677972+AM111D@users.noreply.github.com>
2022-08-04 15:07:24 +0000

    Deploying to gh-pages from @ AM111D/javascript_training@d5979e9d41a75d26bd3ba8767df99e64a2081a67 🚀

---
+++ index.2e4f5e4e.js
@@ -0,0 +1,2 @@
+let o=0;for(;303.91>o;)o+=99.99,o<200&&(o+=9.99,console.log(o));o+=.08*o,console.log("Ваша покупка"+("$"+o.toFixed(2))),o<303.91&&console.log("sorry");
+//# sourceMappingURL=index.2e4f5e4e.js.map
