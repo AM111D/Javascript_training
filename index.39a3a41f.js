3867f0db7ceaeee778e44de2b44562293fe8cee3
AM111D <86677972+AM111D@users.noreply.github.com>
2022-11-12 12:38:55 +0000

    Deploying to gh-pages from @ AM111D/javascript_training@26bf6319aae05f44d5637899f1c59252a574a898 🚀

--- index.39a3a41f.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=303.91,l=0;for(;o>l;)(l+=99.99)<200&&(l+=9.99,console.log(l));l+=.08*l,console.log("Ваша покупка"+("$"+l.toFixed(2))),l<o&&console.log("sorry")}();
-//# sourceMappingURL=index.39a3a41f.js.map
---
+++ index.9cd3f0e4.js
@@ -0,0 +1,2 @@
+!function(){function o(o,e){return 0===e?"Your order is empty!":e>o?"Your order is too large, not enough goods in stock!":"The order is accepted, our manager will contact you"}console.log(o(100,50)),console.log(o(100,130)),console.log(o(70,0)),console.log(o(200,20)),console.log(o(200,250)),console.log(o(150,0))}();
+//# sourceMappingURL=index.9cd3f0e4.js.map
